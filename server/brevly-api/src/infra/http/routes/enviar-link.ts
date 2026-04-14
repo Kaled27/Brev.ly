ad981346026457e954842e36d61219cbe5ca0aeb
Kaled <romulokaled@gmail.com>
2026-04-14 16:22:53 -0300

    feat: swagger configurado

--- server/brevly-api/src/infra/http/routes/enviar-link.ts
+++ server/brevly-api/src/infra/http/routes/enviar-link.ts
@@ -1,7 +1,25 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
 
 export const enviarLinkRoute: FastifyPluginAsyncZod = async (server) => {
-  server.post("/enviar-link", async (request, reply) => {
-    return "Enviar Link!";
-  });
+  server.post(
+    "/enviar-link",
+    {
+      schema: {
+        tags: ["enviar-link"],
+        summary: "Enviar link para encurtamento",
+        body: z.object({
+          link_original: z.string().url(),
+          link_encurtado: z.string().url(),
+        }),
+        response: {
+          201: z.object({ message: z.string() }),
+          409: z.object({ message: z.string() }).describe("Link já enviado."),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ message: "Link enviado com sucesso!" });
+    },
+  );
 };
